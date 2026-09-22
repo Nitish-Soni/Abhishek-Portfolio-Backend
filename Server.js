b048a010fd7ac4b9fda55544198cb55e74e6aed2
Nitish Soni <nitish.soni@okta.com>
2026-09-22 19:19:17 +0530

    Enhanced Backend APIs

--- Server.js
+++ Server.js
@@ -23,14 +23,12 @@ app.use(express.json());
 // Import Routes
 const adminRoutes = require("./Routes/admin");
 const aboutRoutes = require("./Routes/about");
-const portfolioRoutes = require("./Routes/portfolio");
 const inquiryRoutes = require("./Routes/inquiry");
 const subscriberRoutes = require("./Routes/subscriber"); // Added Subscriber routes
 
 // Mount Routes
 app.use("/api/admin", adminRoutes);
 app.use("/api/about", aboutRoutes);
-app.use("/api/portfolio", portfolioRoutes);
 app.use("/api/inquiry", inquiryRoutes);
 app.use("/api/newsletter", subscriberRoutes); // Mounted Newsletter/Subscriber endpoints
 
